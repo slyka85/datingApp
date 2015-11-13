a4b39aeba766bd6dc3c7e63f22c60d1bb6f563f5
Anya Melnyk <gannasachakov@gmail.com>
2015-11-13 13:03:40 -0500

    added avatart and email, reset heroku database

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,9 +17,9 @@ class User < ActiveRecord::Base
 		where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
-      # user.email = auth.info.email
+      user.email = auth.info.email
       user.name = auth.info.name
-      # user.avatar = process_uri(auth['info']['image'] + "?width=9999")
+      user.avatar = process_uri(auth['info']['image'] + "?width=9999")
     end
 		# create(
 		# 	avatar: process_uri(auth['info']['image'] + "?width=9999"),
