1caff19608b645a4251dc79c0066a42ed9d55063
Anya Melnyk <gannasachakov@gmail.com>
2015-11-13 13:16:26 -0500

    added email

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
 		where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
-      # user.email = auth.info.email
+      user.email = auth.info.email
       user.name = auth.info.name
       # user.avatar = auth.info.image
     end
