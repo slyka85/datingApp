8fc9595f7648450055c7067a815c47b147e082b9
Anya Melnyk <gannasachakov@gmail.com>
2015-11-14 00:20:45 -0500

    testing facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,14 +17,14 @@ class User < ActiveRecord::Base
 		where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
-      user.email = auth.info.email
+      # user.email = auth.info.email
       user.name = auth.info.name
-      user.avatar = auth.info.image
+      # user.avatar = auth.info.image
       # user.avatar = process_uri(auth.info.image)
-      user.gender = auth.extra.raw_info.gender
-			user.date_of_birth =  auth.extra.raw_info.birthday,
-			user.location = auth.info.location
-			user.bio = auth.extra.raw_info.bio
+   #    user.gender = auth.extra.raw_info.gender
+			# user.date_of_birth =  auth.extra.raw_info.birthday,
+			# user.location = auth.info.location
+			# user.bio = auth.extra.raw_info.bio
     end
 		# create(
 		# 	avatar: process_uri(auth['info']['image'] + "?width=9999"),
