a1cfce5e2140c7d7f23121443a038a940268503c
Anya Melnyk <gannasachakov@gmail.com>
2015-11-14 10:38:52 -0500

    added image size

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '738025532990730', '762efe7881a1f0690d7a1ccb1d91c78b', 
     {
     scope: 'email,public_profile,user_location,user_birthday,user_about_me',
+    image_size: {width: 400, height: 400},
     info_fields: 'name,email'
   }
 end
