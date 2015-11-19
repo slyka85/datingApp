a0a8b48bffaaf58c87c87993e7164f29ed2aa8dd
Anya Melnyk <gannasachakov@gmail.com>
2015-11-19 16:05:21 -0500

    omniauth ignore state to true

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     {
     scope: 'email,public_profile,user_location,user_birthday,user_about_me',
     image_size: {width: 400, height: 400},
-    info_fields: 'name,email'
+    info_fields: 'name,email',
+    provider_ignores_state: true
   }
 end
