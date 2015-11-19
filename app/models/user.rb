19309038fba923a64ace256d7e99d5bc256f1e07
Anya Melnyk <gannasachakov@gmail.com>
2015-11-19 15:50:39 -0500

    user avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,7 @@ where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
+      user.avatar = process_uri(auth.info.image)
 	end
 end
 
